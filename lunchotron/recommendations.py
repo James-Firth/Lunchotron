a3f14030ba0bcee2eb2ce0228285f873c273baf4
James Firth <james.firth@norimaconsulting.com>
2015-12-09 16:26:30 -0600

    Added fun facts and friendship

--- lunchotron/recommendations.py
+++ lunchotron/recommendations.py
@@ -3,15 +3,30 @@ from slackbot.bot import listen_to
 import subprocess
 import re
 import os
+import random
 import ConfigParser
 
 lunchotron_config = None
 curr_dir =  os.path.dirname(os.path.realpath(__file__))
 
+def get_random_line(file_name):
+	return (random.choice(list(open(file_name))))
+
+def get_food_fact():
+	return get_random_line(os.path.join(curr_dir, "food_fun_facts.txt"))
+
+@respond_to("fact", re.IGNORECASE)
+def fun_facts(message):
+	message.reply("Food Fun Fact: "+get_food_fact())
+
 @respond_to('What.*|where.*', re.IGNORECASE)
 def post_pic(message):
-		message.send('For lunch: You should eat food')
+	message.send('For lunch: You should eat food')
 
-@respond_to('hi', re.IGNORECASE)
+@respond_to('hi|hello', re.IGNORECASE)
 def hello(message):
-		message.send('Hello, I recommend food')
+	message.send('Hello, I recommend food')
+
+@respond_to('friend.*', re.IGNORECASE)
+def friendship(message):
+	message.reply("I'll be your friend!")
